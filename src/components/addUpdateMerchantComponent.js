745af7ead40d2970b6b5ad9f46fe6d8627e516c9
Chirag Swadia <Chirag@druva.local>
2018-03-11 22:19:05 +0530

    add merchant related components renamed to addUpdate as add/update component is same

---
+++ src/components/addUpdateMerchantComponent.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { Field, reduxForm, initialize } from 'redux-form';
+import SelectList from 'react-widgets/lib/SelectList';
+import 'react-widgets/dist/css/react-widgets.css'
+
+class AddUpdateMerchantComponent extends Component{
+
+    constructor(props, context){
+        super(props, context);
+
+        this.state = {
+            show: false
+        }
+    }
+
+    handleShow(){
+        this.setState({show: true})
+        if( this.props.isEditMode ){
+            // transform value of hasPremium from boolean to Yes/No
+            this.props.initialValues['hasPremium'] = this.props.initialValues['hasPremium'] ? 'Yes' : 'No';  
+        }
+        this.props.dispatch(initialize('addupdatemerchant', this.props.initialValues)) // set form values
+    }
+
+    handleClose(){
+        this.setState({show: false})
+    }
+
+    render(){
+        const { handleSubmit, isEditMode=false } = this.props;
+        
+        const renderSelectList = ({ input, data }) =>
+            <SelectList {...input}
+                onBlur={() => input.onBlur()}
+                data={data} />
+
+        return (
+            <div>
+                <Button bsStyle="primary" bsSize={isEditMode ? 'xsmall' : 'sm'} className="mdBtn add" onClick={() => this.handleShow()}>{isEditMode ? 'Update Merchant': 'Add Merchant +'}</Button>
+                {this.state.show &&
+                    <Modal className="modal-container addMerchantModal" 
+                        show={this.state.show} 
+                        animation={true}
+                        onHide={() => this.handleClose()}
+                        >
+
+                        <Modal.Header closeButton>
+                            <Modal.Title>{isEditMode ? 'Update Merchant' : 'Add Merchant'}</Modal.Title>
+                        </Modal.Header>
+
+                        <Modal.Body>
+                            <div className="addMerchantModalContainer">
+                                <form onSubmit={handleSubmit}>
+
+                                    {isEditMode &&
+                                    <Field name="id" className="form-control" component="input" type="hidden" />}
+
+                                    <div className="form-group">
+                                        <label htmlFor="firstname">First Name</label>
+                                        <Field required name="firstname" className="form-control" component="input" type="text" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lastname">Last Name</label>
+                                        <Field required name="lastname" className="form-control" component="input" type="text" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <Field required name="email" className="form-control" component="input" type="email" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Phone Number</label>
+                                        <Field required name="phone" className="form-control" component="input" type="number" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="avatarUrl">Avatar URL</label>
+                                        <Field required name="avatarUrl" className="form-control" component="input" type="text" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="hasPremium">Premium</label>
+                                        <Field
+                                            name="hasPremium"
+                                            component={renderSelectList}
+                                            data={[ 'Yes', 'No' ]}
+                                        />
+                                    </div>
+
+                                    <Button type="submit" bsStyle="primary" className="mdBtn" >{isEditMode ? 'Update Merchant': 'Add Merchant'}</Button>
+                                </form>
+                            </div>
+                        </Modal.Body>
+                    </Modal>
+                }
+            </div>
+        )
+
+    }
+}
+
+AddUpdateMerchantComponent = reduxForm({
+    form: 'addupdatemerchant'
+})(AddUpdateMerchantComponent)
+
+export default AddUpdateMerchantComponent;
---
+++ src/containers/addUpdateMerchantContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {reset} from 'redux-form';
+
+import AddUpdateMerchantComponent from '../components/addUpdateMerchantComponent';
+import { addMerchant, updateMerchant } from '../actions/merchantActions';
+
+//#TODO - Remove if unused
+const mapStateToProps = (state, nextProps) => ({
+   
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    handleSubmit: (formData) => {
+        
+        formData['hasPremium'] = (formData['hasPremium'] === 'Yes') ? true : false; // If user has explicitly selected Yes
+        
+        if( ownProps.isEditMode ){
+            dispatch( updateMerchant(formData) );
+        }else{
+            formData['bids'] = []; // For new record, bids count will be blank
+            formData['timestamp'] = Math.round(+new Date()/1000);
+            dispatch( addMerchant(formData) );
+            dispatch( reset('addmerchant') );
+        }
+        
+    },
+
+});
+
+class AddUpdateMerchantContainer extends Component{
+
+    render(){
+
+        const { handleSubmit, isEditMode=false, initialValues } = this.props;
+        
+        return (
+            <AddUpdateMerchantComponent initialValues={initialValues} onSubmit={handleSubmit} isEditMode={isEditMode} />
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddUpdateMerchantContainer)
